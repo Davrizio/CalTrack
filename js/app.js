6f433c13ceee4c84b779c4816ee6dd65fd7f58e1
Davrizio <davriziod@gmail.com>
2023-04-24 13:10:24 -0400

    added style for calorie over

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ class CalorieTracker {
         this._displayCaloriesComnsumed();
         this._displayCaloriesBurned();
         this._displayCaloriesRemaining();
+        this._displayCaloriesProgress();
     }
 
     addMeal(meal) {
@@ -51,8 +52,28 @@ class CalorieTracker {
     _displayCaloriesRemaining() {
         const caloriesRemainingEl = document.getElementById('calories-remaining');
         const remaining = this._calorieLimit - this._totalCalories;
+        const progressEl = document.getElementById('calorie-progress');
 
         caloriesRemainingEl.innerHTML = remaining;
+
+        if(remaining < 0) {
+            caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-light');
+            caloriesRemainingEl.parentElement.parentElement.classList.add('bg-danger');
+            progressEl.classList.remove('bg-success');
+            progressEl.classList.add('bg-danger');
+        }else{
+            caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-danger');
+            caloriesRemainingEl.parentElement.parentElement.classList.add('bg-light');
+            progressEl.classList.remove('bg-danger');
+            progressEl.classList.add('bg-success');
+        }
+    }
+
+    _displayCaloriesProgress() {
+        const progressEl = document.getElementById('calorie-progress');
+        const percentage = (this._totalCalories / this._calorieLimit) * 100;
+        const width = Math.min(percentage, 100);
+        progressEl.style.width = `${width}%`;
     }
 
     _render() {
@@ -60,6 +81,7 @@ class CalorieTracker {
         this._displayCaloriesComnsumed();
         this._displayCaloriesBurned();
         this._displayCaloriesRemaining();
+        this._displayCaloriesProgress();
     }
 }
 
